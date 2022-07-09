a7bce218ec8775e4756c65a879a6b850321bb539
rohanwinsor <rohan.w.charles@gmail.com>
2022-07-09 11:23:09 +0530

    register R

--- week4/fibonacci.asm
+++ week4/fibonacci.asm
@@ -9,30 +9,30 @@
 // OP-1
 // Get Value from Ram[1] and Store it in RAM[2]
 // Get Value from Ram[1] and Store it in D register
-@1
+@R1
 D=M
 // Get Value from D register and Store it in RAM[2]
-@2
+@R2
 M=D
 // OP-1 Done
 
 // OP-2
 // Get Value from Ram[0] and Add it to RAM[1]
 // Get Value from Ram[1] and Store it in D register
-@0
+@R0
 D=M
 // Get Value from D register and Add it in RAM[1]
-@1
+@R1
 M=M+D
 // OP-2 Done
 
 // OP-3
 // Get Value from Ram[2] and Store it to RAM[0]
 // Get Value from Ram[2] and Store it in D register
-@2
+@R2
 D=M
 // Get Value from D register and Add it in RAM[0]
-@0
+@R0
 M=D
 // OP-3 Done
 
