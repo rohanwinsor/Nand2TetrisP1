9c94c2d72be498a1b6fd0f4608b4aa01d6697f62
rohanwinsor <rohan.w.charles@gmail.com>
2022-07-09 07:52:44 +0530

    Add asm

---
+++ week4/Add.asm
@@ -0,0 +1,10 @@
+// Add value from Ram[0] and Ram[1] and save in Ram[1]
+
+@0 // Select RAM[0] by storing address in A register and value in M register
+D=M // Store Value in RAM[0] in D register
+
+@1 // Select RAM[1]
+D=D+M // Store Value in (D register + RAM[1]) in D register
+
+@1 // Select RAM[0]
+M=D // Store Value in D register in RAM[1]
